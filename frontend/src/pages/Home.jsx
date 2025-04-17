a96ad249711ff24d04326452169b33088487840b
ihatepeanuts32 <naomintuli32@gmail.com>
2025-04-16 20:52:30 -0500

    added logged in users name to home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -7,9 +7,29 @@ import axios from "axios";
 
 const Home = () => {
     const [hackathons, setHackathons] = useState([]);
+    const [user, setUser] = useState('');
 
     //Naomi - Mounting hackathon data being fetched from backend
     useEffect(() => {
+
+        const fetchUserProfile = async () => {
+            try {
+                // Get the token from localStorage or wherever you store it
+                const token = localStorage.getItem('token');
+                
+                if (token) {
+                    const response = await axios.get("http://localhost:3000/api/auth/userProfile", {
+                        headers: {
+                            Authorization: `Bearer ${token}`
+                        }
+                    });
+                    setUser(response.data);
+                }
+            } catch (error) {
+                console.error("Error fetching user profile:", error);
+            }
+        }
+
         const fetchHackathons = async () => {
             try {
                 const response = await axios.get("http://localhost:3000/api/eventBriteHackathons");
@@ -19,14 +39,15 @@ const Home = () => {
                 console.error("Error fetching hackathons:", error);
               }
         }
-    
+        
+        fetchUserProfile();
         fetchHackathons();
     }, []);
 
     return (
         <div>
             <div>
-                <h3>Welcome Vaishali!</h3>
+                <h3>Welcome {user.firstName || "Guest"}!</h3>
             </div>
             <div>
                 <h4>Upcoming Hackathons Near You</h4>
