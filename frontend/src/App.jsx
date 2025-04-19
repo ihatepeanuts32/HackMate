390a0a06ff99551c64dcea6eea78a15839aac272
ifrah-z <reachifrahz@gmail.com>
2025-04-18 23:50:50 -0500

    profile view

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,6 +14,7 @@ import Layout from './Layout';
 import Settings from './pages/Settings';
 import Explore from './pages/Explore';
 import ResetPassword from './pages/ResetPassword';
+import ProfileView from './pages/ProfileView';
 
 function App() {
   return (
@@ -30,6 +31,7 @@ function App() {
           <Route path="/settings" element={<Settings />} />
           <Route path="/explore" element={<Explore />} />
           <Route path="/reset-password" element={<ResetPassword />} />
+          <Route path="/profileView" element={<ProfileView />} />
         </Route>
       </Routes>
     </Router>
