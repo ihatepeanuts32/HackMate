a8ca415002f18f84f0940a6aaf69ed625c3ed578
DhakshinPari <dhakshinpari@gmail.com>
2025-04-30 00:29:19 -0500

    Updating account details

--- frontend/src/pages/AccountDetails.jsx
+++ frontend/src/pages/AccountDetails.jsx
@@ -4,49 +4,7 @@ import axios from "axios";
 import { Link } from 'react-router-dom';
 import '../styles/AccountDetails.css';
 
-//Hrishikesh Srirangam + Dhakshin Parimalakumar
-/*
-const AccountSettings = () => {
-    return (
-        <div>
-            <h2>Your Account</h2>
-            <div className="row-wrapper" style={{ color: 'white', borderRadius:25, background:"#9157ee", minWidth: 800}}>
-                <div className="vertical-line" />
-
-                <div className="split-row">
-                    <div className="left-side">
-                        <h3>Email Address</h3>
-                    </div>
-                    <div className="right-side">
-                        <h3>currentUserId</h3>
-                    </div>
-                </div>
-
-                <div className="split-row">
-                    <div className="left-side">
-                        <h3>Username</h3>
-                    </div>
-                    <div className="right-side">
-                        <h3>Guest</h3>
-                    </div>
-                </div>
-
-                {/* <div className="split-row">
-                    <div className="left-side">
-                        <h3>Account Created</h3>
-                    </div>
-                    <div className="right-side">
-                        <h3>3/31/2025</h3>
-                    </div>
-                </div> }
-            </div>
-        </div>
-    )
-}
-
-export default AccountSettings
-*/
-
+//Dhakshin P
 const AccountSettings = () => {
     const [email, setEmail] = useState('');
     const [username, setUsername] = useState('');
@@ -104,3 +62,46 @@ const AccountSettings = () => {
 };
 
 export default AccountSettings;
+
+//Hrishikesh Srirangam + Dhakshin Parimalakumar
+/*
+const AccountSettings = () => {
+    return (
+        <div>
+            <h2>Your Account</h2>
+            <div className="row-wrapper" style={{ color: 'white', borderRadius:25, background:"#9157ee", minWidth: 800}}>
+                <div className="vertical-line" />
+
+                <div className="split-row">
+                    <div className="left-side">
+                        <h3>Email Address</h3>
+                    </div>
+                    <div className="right-side">
+                        <h3>currentUserId</h3>
+                    </div>
+                </div>
+
+                <div className="split-row">
+                    <div className="left-side">
+                        <h3>Username</h3>
+                    </div>
+                    <div className="right-side">
+                        <h3>Guest</h3>
+                    </div>
+                </div>
+
+                {/* <div className="split-row">
+                    <div className="left-side">
+                        <h3>Account Created</h3>
+                    </div>
+                    <div className="right-side">
+                        <h3>3/31/2025</h3>
+                    </div>
+                </div> }
+            </div>
+        </div>
+    )
+}
+
+export default AccountSettings
+*/
