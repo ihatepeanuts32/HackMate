0c7b3e87b236c872c1af1f899540c600db15b36b
ihatepeanuts32 <naomintuli32@gmail.com>
2025-05-01 22:27:23 -0500

    vite config fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173,  // Set Vite's server to run on port 3000
+    port: 5173,  
     proxy: {
       '/api': {
         target: 'http://localhost:3000', 
