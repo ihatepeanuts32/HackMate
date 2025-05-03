13029c970e8b8eed89d4bb7994e413b65cda638d
Hrishikesh S. <hrishikeshs2005@gmail.com>
2025-05-03 15:05:14 -0500

    Adjusted panel size for account details page

--- frontend/src/pages/AccountDetails.jsx
+++ frontend/src/pages/AccountDetails.jsx
@@ -36,7 +36,7 @@ const AccountSettings = () => {
     return (
         <div>
             <h2>Your Account</h2>
-            <div className="row-wrapper" style={{ color: 'white', borderRadius:25, background:"#9157ee", minWidth: 800}}>
+            <div className="row-wrapper" style={{ color: 'white', borderRadius:25, background:"#9157ee", minWidth: 1000}}>
                 <div className="vertical-line" />
 
                 <div className="split-row">
